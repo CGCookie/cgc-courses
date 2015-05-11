ca855acc2cc82a2d6226357ef52aae14b50bd2a8
Bearded Avenger <email@nickhaskins.com>
2015-05-11 11:20:41 -0500

    doc

--- includes/helpers.php
+++ includes/helpers.php
@@ -3,6 +3,8 @@
 /**
 *	Add a lesson to a course
 *
+*	@param $course_id int id of the course that the lessons are being added to
+*	@param $lesson_id int id of the lesson to add to the course
 *	@since 5.0
 */
 function cgc_course_add_lesson( $course_id = 0, $lesson_id = 0 ) {
@@ -16,8 +18,10 @@ function cgc_course_add_lesson( $course_id = 0, $lesson_id = 0 ) {
 }
 
 /**
-*	Get the course that the lesson is attached to
+*	Get the course that the lesson/quiz/exercise is attached to
 *
+*	@param $lesson_id int id of the post that is attached to the course
+*					      this can be a lesson, quiz, or exercise
 *	@since 5.0
 */
 function cgc_course_get_object_parent( $lesson_id = 0 ){
@@ -32,6 +36,13 @@ function cgc_course_get_object_parent( $lesson_id = 0 ){
 	return $result ? $result[0] : false;
 }
 
+/**
+*	Loop through an array of lessons and attach them to a course
+*
+*	@param $course_id int id of the course that the lessons are being added to
+*	@param $lessons array array of lesson ids
+*	@since 5.0
+*/
 function cgc_course_update_all_lessons( $course_id = 0 , $lessons = array() ) {
 
 	if ( empty( $course_id ) || empty( $lessons ) )
@@ -46,4 +57,24 @@ function cgc_course_update_all_lessons( $course_id = 0 , $lessons = array() ) {
 
 		$db->add_lesson( array( 'course_id' => $course_id, 'lesson_id' => $lesson ) );
 	}
+}
+
+/**
+*	Get the flow that a course is attached to
+*
+*	@since 5.0
+*	@subpackage cgc-flows/public/includes
+*/
+function cgc_course_get_parent_flow( $course_id = 0 ) {
+
+	if ( empty( $course_id ) )
+		$course_id = get_the_ID();
+
+
+	$db = new CGC5_Flows_DB;
+
+	$out = $db->get_flow_of_course( $course_id );
+
+	return $out ? $out[0] : false;
+
 }
